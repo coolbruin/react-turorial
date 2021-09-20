6adfc6e6d5567be1396d59b8e6f8572c145331f7
coolbruin <71934559+coolbruin@users.noreply.github.com>
2021-09-20 17:11:20 +0900

    再レンダリングと副作用を知る(useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColofulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColofulMessage color="bule">元気ですかね？</ColofulMessage>
-      <ColofulMessage color="pink">元気です</ColofulMessage>
+      <ColofulMessage color="blue">元気ですかね？</ColofulMessage>
+      <ColofulMessage color="pink">元気です!</ColofulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>🤗</p>}
     </>
   );
 };
